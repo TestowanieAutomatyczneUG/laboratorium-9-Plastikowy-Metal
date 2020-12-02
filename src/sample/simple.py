6258e2b716f3aa3778d438b11cf126c3c4765cd0
Piotr Szylak <taskercio@gmail.com>
2020-12-02 22:13:23 +0100

    zad 2

--- src/sample/simple.py
+++ src/sample/simple.py
@@ -1,2 +1,12 @@
-def add_one(number):
-    return number + 1
+class Car:
+    def __init__(self):
+        pass
+
+    def needs_fuel(self):
+        pass
+
+    def get_engine_temperature(self):
+        pass
+
+    def drive_to(self, destination):
+        pass
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,16 +1,43 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
-
 import unittest
+from unittest.mock import *
 
-from sample.simple import add_one
+from sample.simple import *
 
 
 class TestSimple(unittest.TestCase):
+    def test_needs_fuel_true(self):
+        test_object = Car()
+        test_object.needs_fuel = Mock(name = 'needs_fuel')
+        test_object.needs_fuel.return_value = True
 
-    def test_add_one(self):
-        self.assertEqual(add_one(5), 6)
+        result = test_object.needs_fuel()
+        self.assertEqual(result, True)
+
+    def test_needs_fuel_false(self):
+        test_object = Car()
+        test_object.needs_fuel = Mock(name = 'needs_fuel')
+        test_object.needs_fuel.return_value = False
+
+        result = test_object.needs_fuel()
+        self.assertEqual(result, False)
+
+    def test_get_engine_temperature(self):
+        test_object = Car()
+        test_object.get_engine_temperature = Mock(name = 'get_engine_temperature')
+        test_object.get_engine_temperature.return_value = 100
+
+        result = test_object.get_engine_temperature()
+        self.assertEqual(result, 100)
+
+    @patch.object(Car, 'drive_to')
+    def test_drive_to_destination(self, mock_method):
+        test_object = Car()
+        dest = 'Sosnowiec'
+        mock_method.return_value = 'Drive to Sosnowiec'
+
+        result = test_object.drive_to(dest)
+        self.assertEqual(result, 'Drive to Sosnowiec')
+
 
 
 if __name__ == '__main__':
